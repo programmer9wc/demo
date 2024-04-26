8f5178d91b6258d64f90fbf3678b2dc61ee49793
programmer9wc <153722629+programmer9wc@users.noreply.github.com>
2024-04-26 16:12:13 +0530

    Update DemoServiceProvider.php

--- src/Providers/DemoServiceProvider.php
+++ src/Providers/DemoServiceProvider.php
@@ -1,1 +1,32 @@
+<?php
 
+namespace Programmer9WC\Demo\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Route;
+
+class DemoServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot(Router $router)
+    {
+        //
+    }
+    
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+    
+
+}
